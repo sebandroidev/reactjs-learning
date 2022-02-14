68294f13f4060706a6959f355dc65ee6bbe1640f
Sebastien NOGBEDJI <nssoftdev@gmail.com>
2022-02-14 12:54:54 +0000

    Arrow functions

---
+++ 02 JavaScript Refresher/arrow-functions.js
@@ -0,0 +1,14 @@
+// Arrow functions are a different syntax
+// for creating a function in Javascript
+
+// Normal function
+function printMyName(name){
+    console.log(name);
+}
+
+// Arrow function
+const printName = (name) => {
+    console.log(name);
+}
+
+printName("Sebastien")
