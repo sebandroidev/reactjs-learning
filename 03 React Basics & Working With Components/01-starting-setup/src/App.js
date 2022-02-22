e794ee94c4b2be799c53ea85d9d8f562ff3000cc
Sebastien NOGBEDJI <nssoftdev@gmail.com>
2022-02-22 14:16:03 +0000

    Splitting components

--- 03 React Basics & Working With Components/01-starting-setup/src/App.js
+++ 03 React Basics & Working With Components/01-starting-setup/src/App.js
@@ -1,10 +1,32 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses"
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: "Car Insurance",
+      amount: 45000,
+      date: new Date(2022, 2, 16)
+    },
+    {
+      id: 'e2',
+      title: "Buy an iPhone",
+      amount: 130000,
+      date: new Date(2022, 3, 25)
+    },
+    {
+      id: 'e3',
+      title: "Pay backend developer",
+      amount: 200000,
+      date: new Date(2022, 3, 8 )
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <Expenses expenses = {expenses} />
     </div>
   );
 }
